0ee09abc9abe1adc8b6e2b8d16d96a93a9643b79
Aarti Yadav <aartiyadav2405@gmail.com>
2024-10-13 14:04:11 +0530

    added env variables

--- src/server.js
+++ src/server.js
@@ -4,19 +4,22 @@ const cors = require("cors");
 const { AuthRouter } = require("./router/AuthRouter");
 const mongoConnect = require("./database/mongooseConnection.js");
 const { ProductRouter } = require("./router/ProductRouter.js");
+const dotenv=require('dotenv')
+
 const app = express();
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 app.use("/auth", AuthRouter);
 app.use("/product", ProductRouter);
+const port=process.env.PORT || 8080;
 app.get("/test", (req, res) => {
   console.log("test api");
   res.send("test api");
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
   mongoConnect();
-  console.log("Server running on 8080 port");
+  console.log(`Server running on ${port} port`);
 });

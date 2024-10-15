ed711aedbd0de7b3dbb7fa9d519b1eb67c7d63fc
Aarti Yadav <aartiyadav2405@gmail.com>
2024-10-15 09:11:22 +0530

    created api to get popular product

--- src/router/ProductRouter.js
+++ src/router/ProductRouter.js
@@ -11,4 +11,21 @@ ProductRouter.get("/get-all-products", async (req, res) => {
     res.status(500).send("Something went wrong");
   }
 });
+
+// popular products
+ProductRouter.get("/get-popular-product", async (req, res) => {
+  // let { rating,reviewCount } = req.headers;
+
+  try {
+    // let data=await Product.find({rating>4})
+    let data = await Product.find({ rating: { $gt: 4 } })
+      .sort({ rating: -1, reviewCount: -1 })
+      .limit(5);
+    console.log(data);
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Something went wrong");
+  }
+});
 module.exports = { ProductRouter };
